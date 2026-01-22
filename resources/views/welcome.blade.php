e7cfb0fb0920813364e7833ff0dc76147f2313dc
fumi udon <116494092+fumi-udon@users.noreply.github.com>
2026-01-22 16:39:51 +0100

    INS FOOTER DEL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,24 @@
         </div>
     </div>
 
-    @include('parts.footer')
+    <footer class="site-footer">
+        <div class="insta-cta-wrapper">
+            <a href="https://www.instagram.com/soya.tunis/" target="_blank" class="insta-btn">
+                <span class="btn-label" id="footer-text">Check Instagram for updates.</span>
+                <span class="btn-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="16" height="16">
+                        <path fill="currentColor"
+                            d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.9 0-184.9zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                    </svg>
+                </span>
+            </a>
+        </div>
+
+        <div class="team-credit">
+            <span class="credit-label">Produced by</span>
+            <span class="credit-name">Bistro Nippon Team</span>
+        </div>
+    </footer>
 
     <script type="module">
         $(function() {
@@ -53,7 +70,6 @@
             const $action = $('#intro-action');
             const $overlay = $('#intro-overlay');
             const $enterBtn = $('#enter-btn');
-            // アニメーション対象から teaser-section を削除
             const $mainElements = $('.main-container, .lang-switcher, .site-footer');
 
             setTimeout(() => $intro1.addClass('fade-up'), 500);
